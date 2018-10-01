24826b619ef37bbb3fbdef0905f0bba3ecc3656a
Malgorzata Ratajczyk <malgorzataratajczyk@wp.pl>
2018-10-01 23:38:55 +0200

    Add fs module and readFile method

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 var http = require('http');
+var fs = require('fs');
 
 var server = http.createServer();
 server.on('request', function (request, response) {
     response.setHeader("Content-Type", "text/html; charset=utf-8");
-    if (request.method === 'GET' && request.url === '/hello') {
-        response.write('<h1>Hello World!</h1>');
-            response.end();
+    if (request.method === 'GET' && request.url === '/') {
+        fs.readFile('./index.html', 'utf-8', function(err, data) {
+            response.write(data);
+
+        });
+        response.end();
     } else {
-            response.statusCode = 404;
-            response.write('<h1>404: Zła ścieżka!</h1>');
+        fs.readFile('./cats.jpg', 'utf-8', function(err, data) {
+            response.write(err, '<img src="./cats.jpg" alt="wrong adress"');
+        });
             response.end();
     }
+    
 });
 
 server.listen(8080);
